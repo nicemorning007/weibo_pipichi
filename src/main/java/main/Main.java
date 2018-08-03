c1bcb7028105e5d3d95206b060fae6d7e5f6200c
nicemorning <oypebook@vip.qq.com>
2018-08-04 07:56:21 +0800

    init project
    Signed-off-by: nicemorning <oypebook@outlook.com>

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018. All Rights Reserved.
+ * FileName: Main.java
+ * Author: NiceMorning
+ */
+
+package main;
+
+/**
+ * Program Entry
+ * <p>
+ * author: nicemorning
+ * version: 1.0
+ * date: 2018/8/4
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
